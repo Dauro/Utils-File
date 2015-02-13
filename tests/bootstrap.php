b84ed16e255f98b88419aba7fc8d3299d5409099
gotardo <contacto@gotardo.es>
2015-02-13 07:24:20 +0100

    PHPUnit configuration for travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,7 @@ ini_set("auto_detect_line_endings", "1");
 
 spl_autoload_register(function ($class){
 
-    $sRelativePath =  '../src' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+    $sRelativePath =  './src' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
     if(file_exists($sRelativePath)) {
         include_once $sRelativePath;
         return true;
